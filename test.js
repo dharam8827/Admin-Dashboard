063da2d971bc6548ae31a851e7297834ad5f06ee
dharam singh <rawatdharam861@gmail.com>
2024-10-01 17:47:07 +0530

    remark

---
+++ test.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'John Doe', rollno: 30, email: 'john@example.com' },
+    { name: 'Jane Smith', rollno: 25, email: 'indore' },
+    { name: 'Alice Johnson', rollno: 35, email: 'alice@example.com' }
+  ];
+  console.log(data.data[2].city);
+  
